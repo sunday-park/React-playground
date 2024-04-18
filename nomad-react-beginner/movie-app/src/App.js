3196007c4a3145e3258957992593bf08146c7d56
sunday-park <sundaypark.it@gmail.com>
2024-04-18 17:35:44 +0900

    movie: Chore: 빌드 및 gh-pages 테스트 업데이트

--- nomad-react-beginner/movie-app/src/App.js
+++ nomad-react-beginner/movie-app/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/Home';
 import Detail from './routes/Detail';
--- nomad-react-beginner/movie-app/src/index.js
+++ nomad-react-beginner/movie-app/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
-  // </React.StrictMode>
+    </React.StrictMode>
+  </BrowserRouter>
 );
